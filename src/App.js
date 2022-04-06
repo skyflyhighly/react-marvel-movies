2e894e9414b990598851c7dd5af9f808da143dcb
skyflyhighly <stepnflors@gmail.com>
2022-04-06 21:49:32 +0545

    creating own API key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect } from "react";
+import "./App.css";
+//  Api key  : 71122cac
+const API_URL = `http://www.omdbapi.com?apikey=71122cac`;
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("SpiderMan");
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Using API</h3>
     </div>
   );
-}
+};
 
 export default App;
