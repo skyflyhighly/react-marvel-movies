5a0fe41f931852e73cb936e0fa302825c3a1efcf
skyflyhighly <stepnflors@gmail.com>
2022-04-07 23:17:26 +0545

    adding searchbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
-//  Api key  : 71122cac
+import SearchBox from "./SearchBox";
+
 const API_URL = `http://www.omdbapi.com?apikey=71122cac`;
 const App = () => {
   const searchMovies = async (title) => {
@@ -13,7 +14,12 @@ const App = () => {
   });
   return (
     <div>
-      <h3>Using API</h3>
+      <div>
+        <h1 className="text-5xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-amber-500 via-yellow-600 to-gray-900 p-12 pb-6">
+          MarvelMovies
+        </h1>
+      </div>
+      <SearchBox />
     </div>
   );
 };
---
+++ src/SearchBox.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className=" relative text-center flex justiy-center items-center bg-gray-900 w-72 mx-auto rounded-sm">
+      <input
+        type="text"
+        placeholder="Search for movies"
+        className="p-2 w-96 text-center bg-gray-900 rounded-md text-amber-400 focus:outline-amber-500 outline-none placeholder-amber-400"
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 fill-amber-500 absolute right-4"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default SearchBox;
