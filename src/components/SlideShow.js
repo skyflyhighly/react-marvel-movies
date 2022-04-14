bd5c03f81f42fa31abc0b959f65f9d945ee13255
skyflyhighly <stepnflors@gmail.com>
2022-04-14 10:05:56 +0545

    making card components scrollable

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const SlideShow = ({ children }) => {
   return (
-    <div className="relative w-full overflow-hidden h-[500px]">
-      <div className={`${children.length > 3 ? "animate" : ""} absolute flex`}>
-        <div className="flex gap-x-4">{children}</div>
+    <div className="relative w-full overflow-hidden h-[500px] overflow-x-scroll">
+      <div className={`${children.length > 3 ? "animate" : ""} absolute flex `}>
+        <div className="flex gap-x-4 ">{children}</div>
       </div>
     </div>
   );
